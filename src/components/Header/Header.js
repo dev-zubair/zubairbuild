5243ed2fe5c9463220dff322e8d768102521d071
zubairhmd <zubairhmd84@gmail.com>
2021-10-08 20:09:14 +0600

    adde new line

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
     return (
         <div className='header'>
             <img src={header} alt="" />
-            <h1 style={{ paddingTop: '20px' }}>The Wheel Summit 2021</h1>
+            <h1 style={{ paddingTop: '20px' }}>The Wheel Summit 2021 for poor people</h1>
             <p>Here are our top donor who will donates for Poor People</p>
             <h3>Total Amount Needed : <FontAwesomeIcon icon={faDollarSign} /> 200 Million</h3>
         </div>
